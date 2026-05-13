7f81e971069094da63019434c39de00874d1ff79
Praize Okidi <praizeokidi@gmail.com>
2026-05-13 12:20:22 +0100

    Fix ES module config for Render

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://rcmi.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         credentials: true
     }
